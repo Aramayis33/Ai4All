b8a4c788aa896e896c658bdfeb44ee5933f87da4
Aramayis33 <aramayishayrapetyan00@gmail.com>
2025-07-20 14:01:30 +0400

    ASd

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-public $baseURL = 'http://' . (getenv('APP_BASEURL') ?: $_SERVER['HTTP_HOST']);
+public string $baseURL = 'https://ai4all-production.up.railway.app/';    
+
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
